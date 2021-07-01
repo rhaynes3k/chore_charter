13ebe2ead9de63714138bbdc1a18a596d6790169
rhaynes3k <rhaynes3k@gmail.com>
2021-06-30 23:56:50 -0500

    removed comments

--- chores_client/src/choreService.js
+++ chores_client/src/choreService.js
@@ -14,17 +14,6 @@ class ChoreService{
         let c = new Chore(chore)
         console.log(c)
         c.domDisplay()
-        // let choreLi = document.createElement('li')
-        // for(const chr of Chore.all){
-        //   let newChore = new Chore(chr)
-        // choreLi.append(c.choreName)
-        // choreLi.append(c.choreDOW)
-        //   choreLi.append(c.user)
-        //   choreUi.append(choreLi)
-          // console.log(choreLi)
-          // let chore_spot = document.getElementById('chore_table_container')
-          // chore_spot.append(choreUi)
-          // console.log(chore_spot)
         }
 
     })
